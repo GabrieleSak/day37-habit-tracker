92ad72e63e206037ae0119cc650c4806c001972e
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2023-01-12 13:36:36 +0200

    Create a graph and post a pixel

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 from auth import *
 
+# create user
+
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
@@ -10,5 +12,38 @@ user_params = {
     "notMinor": "yes"
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+
+# create a graph
+
+graph_endpoint = f"{pixela_endpoint}/{username}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Reading Graph",
+    "unit": "Pages",
+    "type": "int",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": token
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+# post a pixel
+
+pixel_endpoint = f"{pixela_endpoint}/{username}/graphs/{graph_config['id']}"
+
+pixel_config = {
+    "date": "20230112",
+    "quantity": "10",
+    "optionalData": '{"title":"Prisipažįstu", "author":"Jaume Cabré"}'
+}
+
+response = requests.post(url=pixel_endpoint, json=pixel_config, headers=headers)
 print(response.text)
